d1f2ed6962d9eef2fa5a21040c3a1b97dc19dce1
matttungMac <matttung@matttungdeMacBook-Pro.local>
2016-11-30 21:54:32 +0800

    ex02 - add showRandom function

--- ex/script.js
+++ ex/script.js
@@ -2,3 +2,10 @@ function showMsg(msg1, msg2) {
     alert('hello ' + msg1 + ', ' + msg2);
 }
 
+function showRandom(n, m){
+   //var result = Math.floor(Math.random()*(m-n+1)+n); 
+   var result = Math.ceil(Math.random()*(m-n)+n); 
+
+   return result;
+}
+
